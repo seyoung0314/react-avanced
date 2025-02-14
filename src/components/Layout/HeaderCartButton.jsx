1c418e6a140f4fa8132bae913b58acc019437e90
samchi <seyoung0314@gmail.com>
2025-02-14 13:59:26 +0900

    장바구니 추가 시 헤더버튼 이벤트 추가

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,21 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "./CartIcon";
 import styles from "./HeaderCartButton.module.scss";
 import CartContext from "../../context/cart-context";
 
 const HeaderCartButton = ({}) => {
-  const { button, icon, badge } = styles;
+  const { button, icon, badge, bump } = styles;
 
   //useContext 훅은 컨텍스트가 관리하는 데이터를 한번에 가져오는 함수
-  const { openModal } = useContext(CartContext);
+  const { openModal, cartItems } = useContext(CartContext);
+  const [ isBump, setIsBump ] = useState(false);
+
+  const count = cartItems.reduce((acc, curr) => acc + curr.amount, 0);
+
+  useEffect(() => {
+    if(cartItems.length === 0) return;
+    setIsBump(true);
+    
+    // 애니메이션이 끝나면 클래스 제거 
+    const timer = setTimeout(()=>{
+      setIsBump(false);
+    },300);
+
+    return () =>{
+      clearTimeout(timer);
+    }
+  }, [cartItems]);
 
   return (
-    <button className={button} onClick={openModal}>
+    <button className={`${button} ${isBump ? bump : ""}`} onClick={openModal}>
       <span className={icon}>
         <CartIcon />
       </span>
       <span>My Cart</span>
-      <span className={badge}>3</span>
+      <span className={badge}>{count}</span>
     </button>
   );
 };
