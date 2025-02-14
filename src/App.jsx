373b94a170871a04e64933091bd6ea7cc988a04a
samchi <seyoung0314@gmail.com>
2025-02-14 10:03:51 +0900

    컨텍스트 생성과 프로바이더 생성

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Layout/Header";
 import "./App.css";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartContext from "./context/cart-context";
 
 function App() {
   // 장바구니 모달을 열고 닫는 상태변수
@@ -16,14 +17,19 @@ function App() {
     const handleHideCart = () => setCartIsShwon(false);
 
   return (
-    <>
+    // 실제로 공유할 데이터는 value로 전달
+    <CartContext.Provider value={{
+      cartName : "장바구니",
+      amout : 10,
+      isOpen : false
+    }}>
       {cartIsShown && <Cart onClose={handleHideCart}/>}
       <Header onShowCart = {handleShowCart}
       />
       <div id="main">
         <Meals />
       </div>
-    </>
+    </CartContext.Provider>
   );
 }
 
