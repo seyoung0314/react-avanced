6626996728fc93c8c9e4ca4978ab6543b25a93f8
samchi <seyoung0314@gmail.com>
2025-03-05 11:25:07 +0900

    새 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './App.css';
-import ReduxCounter from './redux-practice/components/ReduxCounter';
+import "./App.css";
+import ReduxCounter from "./redux-practice/components/ReduxCounter";
+import Header from "./redux-practice/components/Header";
+import Auth from "./redux-practice/components/Auth";
+import UserProfile from "./redux-practice/components/UserProfile";
 
 const App = () => {
-
   return (
     <>
+      <Header />
+      <Auth />
+      <UserProfile/>
       <ReduxCounter></ReduxCounter>
     </>
   );
