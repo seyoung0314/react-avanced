dbbff9c82802df0405fe6cf983bcfea06caba51a
samchi <seyoung0314@gmail.com>
2025-03-05 09:19:34 +0900

    리덕스 기본세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import React, { useState } from 'react';
 
-import Counter from './optimizing/components/Counter/Counter';
-import Header from './optimizing/components/Header';
-import { log } from './optimizing/log';
 import './App.css';
-import ConfigureCounter from './optimizing/components/Counter/ConfigureCounter';
+import ReduxCounter from './redux-practice/components/ReduxCounter';
 
 const App = () => {
-  log('<App /> rendered');
-
-  const [chosenCount, setChosenCount] = useState(0);
-
-  const setCounterHandler = (number)=>{
-    setChosenCount(number);
-  }
 
   return (
     <>
-      <Header />
-      <main>
-        <ConfigureCounter onSet={setCounterHandler}/>
-        <Counter key={chosenCount} initialCount={chosenCount} />
-      </main>
+      <ReduxCounter></ReduxCounter>
     </>
   );
 };
