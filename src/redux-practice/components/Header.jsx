ba7c0e81f13809102ac04d90d948ef09760ba44e
samchi <seyoung0314@gmail.com>
2025-03-05 11:38:07 +0900

    인증슬라이스 추가(다중 슬라이스사용)

--- src/redux-practice/components/Header.jsx
+++ src/redux-practice/components/Header.jsx
@@ -1,22 +1,27 @@
-import classes from './Header.module.css';
+import { useSelector } from "react-redux";
+import classes from "./Header.module.css";
 
 const Header = () => {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/public'>My Products</a>
-          </li>
-          <li>
-            <a href='/public'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isLoggedIn && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/public">My Products</a>
+            </li>
+            <li>
+              <a href="/public">My Sales</a>
+            </li>
+            <li>
+              <button>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
--- src/redux-practice/store/index.js
+++ src/redux-practice/store/index.js
@@ -2,15 +2,15 @@ import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { createStore } from "redux";
 
 // 액션 타입 상수
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const MULTIPLY = 'MULTIPLY';
-export const TOGGLE = 'TOGGLE';
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+export const MULTIPLY = "MULTIPLY";
+export const TOGGLE = "TOGGLE";
 
 // 상태관리할 데이터의 초기값을 세팅
-const initialState = {
+const initialCounterState = {
   count: 0,
-  showCounter : true,
+  showCounter: true,
 };
 
 // reducer : 상태관리를 위한 함수
@@ -30,29 +30,46 @@ const initialState = {
   prop3: reducers - 기존 리듀서에서 사용하던 내용들(실제 액션)
 */
 const counterSlice = createSlice({
-  name:'counter',
-  initialState,
+  name: "counter",
+  initialState: initialCounterState,
   reducers: {
-    increment(state,_){
+    increment(state, _) {
       state.count++;
     },
-    decrement(state,_){
+    decrement(state, _) {
       state.count--;
     },
-    multiply(state,action){
+    multiply(state, action) {
       state.count *= action.payload.value;
     },
-    toggle(state,_){
+    toggle(state, _) {
       state.showCounter = !state.showCounter;
     },
-  }
+  },
+});
+
+// 인증관련 전역상태 관리 슬라이스
+const initialAuthState = {
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialAuthState,
+  reducers: {},
 });
 
 // 리덕스는 단 하나의 스토어만 사용 (앱당 하나)
 // 스토어는 최상단 컴포넌트에 제공해야한다. (여기선 app.jsx)
-const store = configureStore(
-  {reducer : counterSlice.reducer}
-);
+const store = configureStore({
+
+  //  다중 슬라이스 -> 컴포넌트들이 상태값을 가져갈때 키를 명시해야함
+  // (=> useSelector 시 키를 넣어야함)
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
+});
 
 // 상태를 변경하는 함수들을 모두 내보내기
 export const counterActions = counterSlice.actions;
