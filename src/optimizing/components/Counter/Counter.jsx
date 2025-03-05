e84ecc6d07a94ccb3db0227bbe071491f36099d2
samchi <seyoung0314@gmail.com>
2025-03-05 09:16:11 +0900

    key를 변동값으로 설정했을 때의 렌더링최적화 문제해결

--- src/optimizing/components/Counter/Counter.jsx
+++ src/optimizing/components/Counter/Counter.jsx
@@ -1,14 +1,14 @@
-import React, { useState, memo, useCallback, useEffect } from "react";
+import React, { useState, memo, useCallback, useEffect } from 'react';
 
-import IconButton from "../UI/IconButton";
-import MinusIcon from "../UI/Icons/MinusIcon";
-import PlusIcon from "../UI/Icons/PlusIcon";
-import CounterOutput from "./CounterOutput";
-import { log } from "../../log";
-import CounterHistory from "./CounterHistory";
+import IconButton from '../UI/IconButton';
+import MinusIcon from '../UI/Icons/MinusIcon';
+import PlusIcon from '../UI/Icons/PlusIcon';
+import CounterOutput from './CounterOutput';
+import { log } from '../../log';
+import CounterHistory from './CounterHistory';
 
 const isPrime = (number) => {
-  log("Calculating if is prime number", 2, "other");
+  log('Calculating if is prime number', 2, 'other');
   if (number <= 1) {
     return false;
   }
@@ -25,7 +25,7 @@ const isPrime = (number) => {
 };
 
 const Counter = ({ initialCount }) => {
-  log("<Counter /> rendered", 1);
+  log('<Counter /> rendered', 1);
 
   const initialCountIsPrime = isPrime(initialCount);
 
@@ -34,19 +34,17 @@ const Counter = ({ initialCount }) => {
   // 카운트의 변화를 배열로 추적
   // [0, 1, -1, 1, -1, 1, 1]
   // [{ id: 'x1', value: 0 }, { id: 'x2', value: 1 }, ...]
-  const [counterChanges, setCounterChanges] = useState([
-    { id: Math.random().toString(), value: initialCount },
-  ]);
 
-  useEffect(() => {
-    setCounterChanges([{ id: Math.random().toString(), value: initialCount }]);
-  }, [initialCount]);
+  // useState의 초기값은 최초 렌더링 시에 한번만 세팅되고 다시 변경되지않음
+  const [counterChanges, setCounterChanges] = useState([{ id: Math.random().toString(), value: initialCount }]);
+  
+  // useEffect로 initialCount에 변동에 따라 counterChanges의 초기값을 다시 세팅하는 코드가 필요
+  // useEffect(() => { 
+  //   setCounterChanges([{ id: Math.random().toString(), value: initialCount }]);
+  // }, [initialCount]);
 
   // 현재 카운트의 총합
-  const currentCount = counterChanges.reduce(
-    (acc, curr) => acc + curr.value,
-    0
-  );
+  const currentCount = counterChanges.reduce((acc, curr) => acc + curr.value, 0);
 
   /*
     useCallback hooks은 변경사항이 없는 함수를 재생성하지 않고 재사용하는 훅입니다.
@@ -68,19 +66,23 @@ const Counter = ({ initialCount }) => {
   }, []);
 
   return (
-    <section className="counter">
-      <p className="counter-info">
-        The initial counter value was <strong>{initialCount}</strong>. It{" "}
-        <strong>is {initialCountIsPrime ? "a" : "not a"}</strong> prime number.
+    <section className='counter'>
+      <p className='counter-info'>
+        The initial counter value was <strong>{initialCount}</strong>. It{' '}
+        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
       </p>
       <p>
-        <IconButton icon={MinusIcon} onClick={decrementHandler}>
+        <IconButton
+          icon={MinusIcon}
+          onClick={decrementHandler}>
           Decrement
         </IconButton>
 
         <CounterOutput value={currentCount} />
 
-        <IconButton icon={PlusIcon} onClick={incrementHandler}>
+        <IconButton
+          icon={PlusIcon}
+          onClick={incrementHandler}>
           Increment
         </IconButton>
       </p>
